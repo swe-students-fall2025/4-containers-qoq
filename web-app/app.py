faa684d3f59ee219c10cd2b8db3f623c37578a60
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-13 22:33:25 -0500

    fix --black formatting

--- web-app/app.py
+++ web-app/app.py
@@ -4,15 +4,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """Index page."""
     return render_template("index.html")
 
-@app.route('/dashboard')
+
+@app.route("/dashboard")
 def dashboard():
     """Dashboard page."""
     return render_template("dashboard.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
