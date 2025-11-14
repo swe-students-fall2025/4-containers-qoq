be324259824539ca971080c2139107ff5c6d2370
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-13 22:31:01 -0500

    fix --flask routes and add dashboard.html placeholder

--- web-app/app.py
+++ web-app/app.py
@@ -4,12 +4,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/')
 def index():
-    """index page."""
+    """Index page."""
     return render_template("index.html")
 
-
+@app.route('/dashboard')
 def dashboard():
-    """Dashboard page"""
+    """Dashboard page."""
     return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
