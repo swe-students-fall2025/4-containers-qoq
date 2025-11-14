37ef81f3bb70a1293146417a8ce562a127dde5fd
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-13 21:34:50 -0500

    set up pylint and add index.html

--- web-app/app.py
+++ web-app/app.py
@@ -1,14 +1,12 @@
+"""Basic Flask web app Setup"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 def index():
+    """index page."""
     return render_template('index.html')
 
-@app.route('/dashboard')
 def dashboard():
+    """Dashboard page"""
     return render_template('dashboard.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
