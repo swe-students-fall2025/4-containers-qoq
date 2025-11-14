a6e667f317ccd8ec8151d8dd2d865cb98af33333
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-13 22:13:31 -0500

    format web-app to pass CI lint test

--- web-app/app.py
+++ web-app/app.py
@@ -1,12 +1,15 @@
 """Basic Flask web app Setup"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 def index():
     """index page."""
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 def dashboard():
     """Dashboard page"""
-    return render_template('dashboard.html')
+    return render_template("dashboard.html")
